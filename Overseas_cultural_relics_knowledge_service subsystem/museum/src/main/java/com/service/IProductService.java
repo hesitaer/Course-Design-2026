a7d88c4ff86109b7f8cd2e9ce063f78635a5f579
savior69 <2023040112@buct.edu.cn>
2026-05-17 13:03:07 +0800

    Update IProductService.java

--- Overseas_cultural_relics_knowledge_service subsystem/museum/src/main/java/com/service/IProductService.java
+++ Overseas_cultural_relics_knowledge_service subsystem/museum/src/main/java/com/service/IProductService.java
@@ -1,10 +1,22 @@
 package com.service;
 
 import com.entity.Product;
+import com.entity.ProductQueryDTO;
 
 import java.math.BigInteger;
+import java.util.List;
+import java.util.Map;
 
 public interface IProductService {
     Product findByProduct_id(BigInteger id);
-
+    
+    List<Product> findByConditions(ProductQueryDTO queryDTO);
+    
+    Map<String, Object> findByConditionsWithPage(ProductQueryDTO queryDTO);
+    
+    List<Product> searchByKeyword(String keyword);
+    
+    String exportToCSV(ProductQueryDTO queryDTO);
+    
+    String exportToJSON(ProductQueryDTO queryDTO);
 }
