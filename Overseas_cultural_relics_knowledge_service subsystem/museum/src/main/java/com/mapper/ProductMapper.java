588d7344ea21c9632f9918e9d26f863287371221
savior69 <2023040112@buct.edu.cn>
2026-05-17 13:01:04 +0800

    Add methods for product queries in ProductMapper

--- Overseas_cultural_relics_knowledge_service subsystem/museum/src/main/java/com/mapper/ProductMapper.java
+++ Overseas_cultural_relics_knowledge_service subsystem/museum/src/main/java/com/mapper/ProductMapper.java
@@ -1,12 +1,26 @@
 package com.mapper;
 
 import com.entity.Product;
+import com.entity.ProductQueryDTO;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
 import java.math.BigInteger;
+import java.util.List;
+
 @Repository
 @Mapper
 public interface ProductMapper {
     Product findByProduct_id(BigInteger id);
+    
+    List<Product> findByConditions(ProductQueryDTO queryDTO);
+    
+    List<Product> findByConditionsWithPage(@Param("queryDTO") ProductQueryDTO queryDTO, 
+                                          @Param("offset") Integer offset, 
+                                          @Param("pageSize") Integer pageSize);
+    
+    Integer countByConditions(ProductQueryDTO queryDTO);
+    
+    List<Product> searchByKeyword(@Param("keyword") String keyword);
 }
