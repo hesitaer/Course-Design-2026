bbf1c58182e4ec49c57ac4a284553268007d8a86
savior69 <2023040112@buct.edu.cn>
2026-05-27 22:49:01 +0800

    Create ProductQueryDTO.java

---
+++ Overseas_cultural_relics_knowledge_service subsystem/museum/src/main/java/com/entity/ProductQueryDTO.java
@@ -0,0 +1,22 @@
+package com.entity;
+
+import lombok.Data;
+
+@Data
+public class ProductQueryDTO {
+    private String keyword;           // 关键词搜索
+    private String title;             // 文物名称
+    private String artist;            // 作者
+    private String dynasty;           // 朝代
+    private String period;            // 时期
+    private String type;              // 类型
+    private String material;          // 材质
+    private String culture;           // 文化
+    private String museum;            // 博物馆名称
+    private Integer museumId;         // 博物馆ID：1史密森尼 2哈佛 3波士顿MFA
+    
+    private Integer pageNum = 1;      // 页码
+    private Integer pageSize = 10;    // 每页大小
+    private String sortField = "title"; // 排序字段
+    private String sortOrder = "asc"; // 排序方向
+}
