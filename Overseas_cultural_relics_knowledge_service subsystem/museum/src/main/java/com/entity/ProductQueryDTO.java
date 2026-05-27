1f75fcef0b2f96c574837b3d0d597b524d64a69f
savior69 <2023040112@buct.edu.cn>
2026-05-27 22:25:01 +0800

    Update ProductQueryDTO.java

--- Overseas_cultural_relics_knowledge_service subsystem/museum/src/main/java/com/entity/ProductQueryDTO.java
+++ Overseas_cultural_relics_knowledge_service subsystem/museum/src/main/java/com/entity/ProductQueryDTO.java
@@ -4,20 +4,19 @@ import lombok.Data;
 
 @Data
 public class ProductQueryDTO {
-    private String keyword;
-    private String medium;
-    private String objectType;
-    private String timePeriod;
-    private String geography;
-    private String museum;
-    private String cat1;
-    private String cat2;
-    private String cat3;
-    private String makersName;
-    private String objectName;
+    private String keyword;           // 关键词搜索
+    private String title;             // 文物名称
+    private String artist;            // 作者
+    private String dynasty;           // 朝代
+    private String period;            // 时期
+    private String type;              // 类型
+    private String material;          // 材质
+    private String culture;           // 文化
+    private String museum;            // 博物馆名称
+    private Integer museumId;         // 博物馆ID：1史密森尼 2哈佛 3波士顿MFA
     
-    private Integer pageNum = 1;
-    private Integer pageSize = 10;
-    private String sortField = "id";
-    private String sortOrder = "asc";
+    private Integer pageNum = 1;      // 页码
+    private Integer pageSize = 10;    // 每页大小
+    private String sortField = "title"; // 排序字段
+    private String sortOrder = "asc"; // 排序方向
 }
